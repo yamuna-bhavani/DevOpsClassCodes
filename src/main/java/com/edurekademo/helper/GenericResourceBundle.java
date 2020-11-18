8a2da3034cade34223e454fd97c8f69682fe36c6
yamuna <yamuna.chinni06@gmail.com>
2020-11-18 15:48:12 +0530

    Test

--- src/main/java/com/edurekademo/helper/GenericResourceBundle.java
+++ src/main/java/com/edurekademo/helper/GenericResourceBundle.java
@@ -8,13 +8,11 @@ public class GenericResourceBundle {
 		ResourceBundle rb = ResourceBundle.getBundle("ResourceBundle");
 		Enumeration <String> keys = rb.getKeys();
 		String value="";
+		//test
 		while (keys.hasMoreElements()) {
 			
 			String key =  keys.nextElement();
 			
-			if(key.equalsIgnoreCase(source)){
-				value = rb.getString(key);
-			}
 		}
 		return value;
 	}
